8d9b4b478172a5f736be9c89fddfc83d3234d384
lifochiu <lifochiu.ca@gmail.com>
2025-11-30 21:24:49 -0500

    merge calendar page and analysis page

--- script/analysis.js
+++ script/analysis.js
@@ -1,3 +1,27 @@
+let savedTasks = JSON.parse(localStorage.getItem('toDoList')) || [];
+let closedTask = 0;
+let inProgressTask = 0;
+let delayedTask = 0;
+const THREE_DAYS_IN_MS = 3 * 24 * 60 * 60 * 1000;
+const now = new Date().getTime();
+
+savedTasks.forEach(element => {
+    let timeDiff = now - element[4];
+    if (element[2] == "Open") { //task at close status
+        closedTask++;
+    } else { //task at open status
+        inProgressTask++
+    }
+    if (timeDiff > THREE_DAYS_IN_MS) {
+        delayedTask++;
+    }
+});
+
+document.getElementById('inProgressItem').textContent = `In Progress: ${inProgressTask} item`;
+document.getElementById('closedItem').textContent = `Complete: ${closedTask} item`;
+document.getElementById('delayItem').textContent = `Delayed: ${delayedTask} item`;
+
+
 document.addEventListener("DOMContentLoaded", function () {
 
     const ctx = document.getElementById("taskChart");
@@ -8,27 +32,84 @@ document.addEventListener("DOMContentLoaded", function () {
             labels: ["Delayed", "In Progress", "Complete"],
             datasets: [
                 {
-                    data: [2, 2, 2],
+                    data: [delayedTask, inProgressTask, closedTask],
                     borderWidth: 5,
                     borderColor: "black",
                     backgroundColor: ["pink", "purple", "lightskyblue"]
                 }
             ]
         },
-
         options: {
             responsive: true,
-
             plugins: {
                 legend: {
-                    position: "top",   // <-- this is correct
-                    labels: {
-                        font: {
-                            size: 18    // <-- bigger labels
-                        }
-                    }
+                    position: "top",
+                    labels: { font: { size: 18 } }
                 }
             }
         }
     });
+
+    
+    let mediaRecorder;
+    let audioChunks = [];
+
+    const recordBtn = document.getElementById("recordBtn");
+
+    recordBtn.addEventListener("click", async () => {
+
+        if (mediaRecorder && mediaRecorder.state === "recording") {
+            mediaRecorder.stop();
+            recordBtn.textContent = "Start Recording";
+            recordBtn.classList.remove("recording");
+            return;
+        }
+
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+
+        mediaRecorder = new MediaRecorder(stream);
+
+        mediaRecorder.ondataavailable = event => audioChunks.push(event.data);
+        mediaRecorder.onstop = () => {
+            const audioBlob = new Blob(audioChunks, { type: "audio/webm" });
+            audioChunks = [];
+
+            
+
+         const audioURL = URL.createObjectURL(audioBlob);
+
+         const recordingDiv = document.createElement("div");
+         recordingDiv.classList.add("audio-recording");
+
+         const newAudio = document.createElement("audio");
+         newAudio.controls = true;
+         newAudio.src = audioURL;
+
+         const a = document.createElement("a");
+         a.href = audioURL;
+         a.download = "recording.webm";
+         a.style.marginLeft = "10px";
+
+         const deleteBtn = document.createElement("button");
+         deleteBtn.textContent = "Delete";
+         deleteBtn.style.marginLeft = "10px";
+         deleteBtn.classList.add("delete-button");
+         deleteBtn.addEventListener("click", () => {
+            recordingDiv.remove();
+         });
+
+         recordingDiv.appendChild(newAudio);
+         recordingDiv.appendChild(a);
+         recordingDiv.appendChild(deleteBtn);
+
+         const container = document.getElementById("audioContainer");
+         container.appendChild(recordingDiv);
+        };
+
+
+        mediaRecorder.start();
+        recordBtn.textContent = "Stop Recording";
+        recordBtn.classList.add("recording");
+    });
+
 });
--- script/todoPage.js
+++ script/todoPage.js
@@ -1,20 +1,35 @@
 const taskInput = document.getElementById('taskInput');
 const taskCreateBtn = document.getElementById('taskCreateBtn');
 const contentArea = document.getElementById('contentArea');
-let taskID = 1;
 let savedTasks = JSON.parse(localStorage.getItem('toDoList')) || [];
+let taskID = savedTasks.length + 1;
+
 
 savedTasks.forEach(element => {
-    createDiv(element);
+    createDiv(element[0], element[1]);
+    if (element[2] == "Open") { //task at close status
+        document.getElementById(`${element[0]}`).textContent = `#${element[0]}`;
+        document.getElementById(`taskContent${element[0]}`).style.textDecoration = "line-through";
+        document.getElementById(`closeBtn${element[0]}`).textContent = "Open";
+        document.getElementById(`currentDate${element[0]}`).textContent = element[3];
+    } else { //task at open status
+        document.getElementById(`${element[0]}`).textContent = `#${element[0]}`;
+        document.getElementById(`taskContent${element[0]}`).style.textDecoration = "none";
+        document.getElementById(`currentDate${element[0]}`).textContent = element[3];
+    }
 });
 
 taskCreateBtn.addEventListener('click', () => {
-
+    
     if (taskInput.value == "") {
         window.alert("Error, please input text");
     } else {
-        createDiv(taskInput.value);
-        savedTasks.push(taskInput.value);
+        let date = new Date();
+        let microSecond = date.getTime();
+        date = date.toLocaleString('en-US');
+        let taskArray = [taskID, taskInput.value, "Close", date, microSecond];
+        createDiv(taskID, taskInput.value, date);
+        savedTasks.push(taskArray);
         localStorage.setItem('toDoList', JSON.stringify(savedTasks));
     }
     taskID++;
@@ -26,15 +41,15 @@ taskCreateBtn.addEventListener('click', () => {
 
 console.log(savedTasks);
 
-function createDiv(text) {
-    const date = new Date();
+function createDiv(id, text, date) {
+    // const date = new Date();
     let taskHTML =
     ` <div class="taskArea" id="task">
-    <p class="taskContentNumber" id="task">#</p>
-    <p class="taskContent" id="taskContent">${text}</p>
-    <p class="currentDate" id="currentDate">${date.toLocaleString()}</p>
+    <p class="taskContentNumber" id="${id}">#${id}</p>
+    <p class="taskContent" id="taskContent${id}">${text}</p>
+    <p class="currentDate" id="currentDate${id}">${date}</p>
     <button class="editBtn" id="editBtn">Edit</button>
-    <button class="closeBtn" id="closeBtn">Close</button>
+    <button class="closeBtn" id="closeBtn${id}">Close</button>
   </div>`;
     contentArea.insertAdjacentHTML('beforeend', taskHTML);
 }
@@ -42,12 +57,14 @@ function createDiv(text) {
 //edit button and close button for each task
 contentArea.addEventListener('click', function (event) {
     if (event.target.classList.contains('editBtn')) {
-        // console.log(event.target.id);
         const taskArea = event.target.closest('.taskArea');
         const content = taskArea.querySelector('.taskContent');
-        const newContent = prompt("Input new content: ", content.textContent);
+        const taskId = taskArea.querySelector('.taskContentNumber');
+        const newContent = prompt("Input new content: ");
         if (newContent !== null && newContent.trim() !== "") {
             content.textContent = newContent;
+            savedTasks[taskId.id - 1][1] = newContent;
+            localStorage.setItem('toDoList', JSON.stringify(savedTasks));
         }
     }
     if (event.target.classList.contains('closeBtn')) {
@@ -55,14 +72,21 @@ contentArea.addEventListener('click', function (event) {
         // event.target.closest('.taskArea').remove();
         const taskArea = event.target.closest('.taskArea');
         const content = taskArea.querySelector('.taskContent');
+        const taskId = taskArea.querySelector('.taskContentNumber');
         const closeBtn = taskArea.querySelector('.closeBtn');
         if (closeBtn.textContent == "Close") {
             content.style.textDecoration = "line-through";
             closeBtn.textContent = "Open";
+            savedTasks[taskId.id - 1][2] = "Open";
+            localStorage.setItem('toDoList', JSON.stringify(savedTasks));
+            console.log(savedTasks);
+
         } else {
             content.style.textDecoration = "none";
             closeBtn.textContent = "Close";
+            savedTasks[taskId.id - 1][2] = "Close";
+            localStorage.setItem('toDoList', JSON.stringify(savedTasks));
+            console.log(savedTasks);
         }
-
     }
 })
